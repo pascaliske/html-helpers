e8b06a1003da1cefca68b83dcbbcd25b16b39e87
Pascal Iske <info@pascal-iske.de>
2018-08-20 09:24:38 +0200

    test(modifiers): add tests for empty arguments and new themes string argument

--- tests/styles.test.js
+++ tests/styles.test.js
@@ -1,6 +1,13 @@
 import test from 'ava'
 import * as exports from '../dist'
 
+test('return empty space', async t => {
+    const expected = ''
+    const result = exports.modifiers()
+
+    t.is(result, expected)
+})
+
 test('return modifiers with namespace', async t => {
     const expected = 'cmp-demo cmp-demo--foo cmp-demo--baz'
     const result = exports.modifiers('cmp-demo', {
@@ -19,6 +26,24 @@ test('return modifiers without namespace', async t => {
         bar: false,
         baz: true
     })
+
+    t.is(result, expected)
+})
+
+test('return modifiers with theme string and classes map', async t => {
+    const expected = 'cmp-demo cmp-demo--primary cmp-demo--full-width cmp-demo--foo cmp-demo--baz'
+    const result = exports.modifiers('cmp-demo', 'primary,full-width', {
+        foo: true,
+        bar: false,
+        baz: true
+    })
+
+    t.is(result, expected)
+})
+
+test('return modifiers with theme string but without classes map', async t => {
+    const expected = 'cmp-demo cmp-demo--primary cmp-demo--full-width'
+    const result = exports.modifiers('cmp-demo', 'primary,full-width')
 
     t.is(result, expected)
 })
