5fef15a4d2c4a2a3fde67c81d9f7b30e60727733
Pascal Iske <info@pascal-iske.de>
2018-10-02 23:10:18 +0200

    doc: add comments

--- src/styles.ts
+++ src/styles.ts
@@ -5,8 +5,31 @@ export interface ClassesMap {
     [key: string]: boolean
 }
 
+/**
+ * Returns the final class string for the given class name maps.
+ *
+ * @param map -
+ * @returns A class string
+ */
 export function modifiers(map: ClassesMap): string
+
+/**
+ * Returns the final class string for the given class name maps.
+ *
+ * @param namespace - A namespace for prepending.
+ * @param map - A class name map.
+ * @returns A class string
+ */
 export function modifiers(namespace: string, map: ClassesMap): string
+
+/**
+ * Returns the final class string for the given class name maps.
+ *
+ * @param namespace - A namespace for prepending.
+ * @param themes - A string of comma separated modifier classes.
+ * @param map - A class name map.
+ * @returns A class string
+ */
 export function modifiers(namespace: string, themes: string, map: ClassesMap): string
 export function modifiers(
     namespaceOrMap: string | ClassesMap,
