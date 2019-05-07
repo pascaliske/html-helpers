10a2c3ffcc636d08e4fd7f8cb248691a72160001
Pascal Iske <info@pascal-iske.de>
2019-05-07 10:13:09 +0200

    feat(styles): add join function for joining array of class names to one class string

--- tests/styles.test.js
+++ tests/styles.test.js
@@ -1,14 +1,14 @@
 import test from 'ava'
-import { modifiers } from '../dist'
+import { modifiers, join } from '../dist'
 
-test('return empty space', async t => {
+test('modifiers: return empty space', async t => {
     const expected = ''
     const result = modifiers()
 
     t.is(result, expected)
 })
 
-test('return modifiers with namespace', async t => {
+test('modifiers: return modifiers with namespace', async t => {
     const expected = 'cmp-demo cmp-demo--foo cmp-demo--baz'
     const result = modifiers('cmp-demo', {
         foo: true,
@@ -19,7 +19,7 @@ test('return modifiers with namespace', async t => {
     t.is(result, expected)
 })
 
-test('return modifiers without namespace', async t => {
+test('modifiers: return modifiers without namespace', async t => {
     const expected = 'foo baz'
     const result = modifiers({
         foo: true,
@@ -30,7 +30,7 @@ test('return modifiers without namespace', async t => {
     t.is(result, expected)
 })
 
-test('return modifiers with theme string and classes map', async t => {
+test('modifiers: return modifiers with theme string and classes map', async t => {
     const expected = 'cmp-demo cmp-demo--primary cmp-demo--full-width cmp-demo--foo cmp-demo--baz'
     const result = modifiers('cmp-demo', 'primary,full-width', {
         foo: true,
@@ -41,7 +41,7 @@ test('return modifiers with theme string and classes map', async t => {
     t.is(result, expected)
 })
 
-test('return modifiers with empty theme string and classes map', async t => {
+test('modifiers: return modifiers with empty theme string and classes map', async t => {
     const expected = 'cmp-demo cmp-demo--foo cmp-demo--baz'
     const result = modifiers('cmp-demo', '', {
         foo: true,
@@ -52,7 +52,7 @@ test('return modifiers with empty theme string and classes map', async t => {
     t.is(result, expected)
 })
 
-test('return modifiers with undefined theme string and classes map', async t => {
+test('modifiers: return modifiers with undefined theme string and classes map', async t => {
     const expected = 'cmp-demo cmp-demo--foo cmp-demo--baz'
     const result = modifiers('cmp-demo', undefined, {
         foo: true,
@@ -63,9 +63,44 @@ test('return modifiers with undefined theme string and classes map', async t => 
     t.is(result, expected)
 })
 
-test('return modifiers with theme string but without classes map', async t => {
+test('modifiers: return modifiers with theme string but without classes map', async t => {
     const expected = 'cmp-demo cmp-demo--primary cmp-demo--full-width'
     const result = modifiers('cmp-demo', 'primary,full-width')
+
+    t.is(result, expected)
+})
+
+test('join: returns an empty class string', async t => {
+    const expected = ''
+    const result = join()
+
+    t.is(result, expected)
+})
+
+test('join: returns an class string with two given classes', async t => {
+    const expected = 'foo bar'
+    const result = join('foo', 'bar')
+
+    t.is(result, expected)
+})
+
+test('join: returns an class string with one of two given duplicate classes', async t => {
+    const expected = 'foo'
+    const result = join('foo', 'foo')
+
+    t.is(result, expected)
+})
+
+test('join: returns an class string with multiple classes in arrays', async t => {
+    const expected = 'foo bar baz'
+    const result = join('foo', ['bar'], 'baz')
+
+    t.is(result, expected)
+})
+
+test('join: returns an class string with multiple classes in arrays and duplicates', async t => {
+    const expected = 'foo bar baz'
+    const result = join('foo', ['bar'], 'baz', ['foo'])
 
     t.is(result, expected)
 })
